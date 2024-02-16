094bd266c9287519fe41072a5352fb7b517a6ec2
jose-izarra <jaiza0912@gmail.com>
2024-02-16 19:12:18 +0100

    initial commit

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+
+const app = express();
+
+require('dotenv').config();
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World');
+})
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
