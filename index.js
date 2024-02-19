c29096bde22d74de89ed9435cb27536a6433b9c9
jose-izarra <jaiza0912@gmail.com>
2024-02-19 18:30:02 +0100

    cors added

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-
-
 const login = require('./api/login');
 const callback = require('./api/callback');
 const newToken = require('./api/newToken');
 
-// app.use(cors())
+
+app.use(cors())
 // app.use(express.json({ extended: false }));
 
 app.use('/api/login', login);
